7117a3f3ea07a933f61769435c35fa0d8184a6e8
Jose Samuel <js.dev97@gmail.com>
2023-01-22 22:13:28 -0300

    wrong code again(setData)

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 const button = document.querySelector("header button")
 
-button.addEventListener("click", add)//quando clica no botão,a função add () abaixo é ativada
-form.addEventListener("change", save) 
+button.addEventListener("click", add) //quando clica no botão,a função add () abaixo é ativada
+form.addEventListener("change", save)
 
 function add() {
   const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
   const dayExists = nlwSetup.dayExists(today)
-  
+
   if (dayExists) {
     alert("Dia já incluso")
     return
@@ -23,7 +23,7 @@ function save() {
 }
 
 const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
-nlwSetup.SetData(data)
+nlwSetup.setData(data)
 nlwSetup.load()
 //const data = {
 //  run: ['01-01', '01-02', '01-06', '01-07','01-08'],
